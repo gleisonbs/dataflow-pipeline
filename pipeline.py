c551f961836e66929d96dac8402d2b9ddd23173e
Gleison Batista <gleisonbs@gmail.com>
2019-10-11 19:24:23 -0300

    Fix pipeline

--- pipeline.py
+++ pipeline.py
@@ -3,22 +3,35 @@ from apache_beam.io import ReadFromText
 from apache_beam.io import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions
 
-pipeline_options = {
-'project': 'serious-mariner-255222',
-    'staging_location': 'gs://mybucket20b40d8c/staging',
-    'runner': 'DataflowRunner',
-    'job_name': 'my-job-is-nameless-2t4tfw4e',
-    'disk_size_gb': 100,
-    'temp_location': 'gs://mybucket20b40d8c/temp',
-    'save_main_session': True
-}
+def run():
+    pipeline_options = {
+        'project': 'serious-mariner-255222',
+        'staging_location': 'gs://mybucket20b40d8c/staging',
+        'temp_location': 'gs://mybucket20b40d8c/temp',
+        'template_location': 'gs://mybucket20b40d8c/templates/number_lines_temp',
+        'runner': 'DataflowRunner',
+        'job_name': 'my-dataflow-job',
+        'output': 'gs://mybucket20b40d8c/output/new-data.txt',
+        'input': 'gs://mybucket20b40d8c/data.txt',
+    }
 
-options = PipelineOptions.from_dictionary(pipeline_options)
-p = beam.Pipeline(options=options)
+    def remove_new_line(line):
+        return line.strip('\n')
 
-lines = (p
-        | ReadFromText('gs://mybucket20b40d8c/data.csv')
-        | WriteToText('gs://mybucket20b40d8c/my-data.csv'))
+    def append_number(line):
+        return f'1 - {line}'
 
-result = p.run()
-result.wait_until_finish()
+    pipeline_options = PipelineOptions.from_dictionary(pipeline_options)
+
+    p = beam.Pipeline(options=pipeline_options)
+
+    output = (p | 'read' >> ReadFromText('gs://mybucket20b40d8c/data.txt')
+                | 'remove_new_lines' >> beam.Map(remove_new_line)
+                | 'append_number' >> beam.Map(append_number)
+                | 'write' >> WriteToText('gs://mybucket20b40d8c/output/new-data.txt'))
+
+    result = p.run()
+    result.wait_until_finish()
+
+if __name__ == '__main__':
+    run()
